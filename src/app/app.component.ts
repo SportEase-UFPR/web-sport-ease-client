502a4c5453891110106105390487777fa43b0956
MatheusKerscher <kerschermatheus12@gmail.com>
2023-09-07 15:20:35 -0300

    ajustes no layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component, DoCheck, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
 import { LocalStorageService } from './shared/services/local-storage/local-storage.service';
 import { environment as env } from 'src/environments/environment';
 import { UsuarioLs } from './shared/models/usuario-ls/usuario-ls.model';
@@ -11,36 +12,33 @@ import { UsuarioLs } from './shared/models/usuario-ls/usuario-ls.model';
 export class AppComponent implements OnInit {
   constructor(private lsService: LocalStorageService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    window.onresize = () => {
+      this.formatLayout();
+    };
+  }
 
-  selectLayout(): boolean {
-    if (!this.isLogado()) return true;
-
+  formatLayout() {
     const alturaJanela = window.innerHeight;
     const alturaMinima = Math.round(alturaJanela * 0.05);
     const alturaMaxima = Math.round(alturaJanela * 0.95) - alturaMinima;
-    const cabecalho = document.getElementById('cabecalho');
-    const menu = document.getElementById('menu');
-    const paginas = document.getElementById('paginas');
+    const cabecalho = document?.getElementById('cabecalho');
+    const menu = document?.getElementById('menu');
+    const paginas = document?.getElementById('paginas');
 
     if (cabecalho) cabecalho.style.minHeight = `${alturaMinima}px`;
 
     if (menu) menu.style.minHeight = `${alturaMaxima}px`;
 
     if (paginas) paginas.style.minHeight = `${alturaMaxima}px`;
-
-    return false;
   }
 
   isLogado(): boolean {
     const lsDados: UsuarioLs = this.lsService.get(env.ls_token);
+    this.formatLayout();
 
     if (lsDados) {
-      if (lsDados.usuarioLogado) {
-        return true;
-      } else {
-        return false;
-      }
+      return lsDados.usuarioLogado!;
     } else {
       return false;
     }
